77020e14c83ac10c4e5d09c20f08372f8133997b
Kavinder Dhaliwal and Shoabe Shariff <pair+kdhaliwal+sshariff@pivotal.io>
2015-10-20 14:31:10 -0700

    Compile and archive libhdfs3 to s3 resource [#105025150]
    - Archived tar contains libhdfs3 version and git sha in filename

--- ci/compile.bash
+++ ci/compile.bash
@@ -2,11 +2,46 @@
 
 set -euo pipefail
 
+ensure_git_installed() {
+  if ! which git 2> /dev/null; then
+    yum install -y git
+  fi
+}
+
+get_libhdfs3_version() {
+  local dir_to_tar
+  dir_to_tar="$1"
+  cat "${dir_to_tar}/lib/pkgconfig/libhdfs3.pc" | grep Version | cut -d ' ' -f2
+}
+
+generate_package_name() {
+  local git_hash
+  git_hash=$(git log -1 --oneline | head -1 | cut -d ' ' -f1)
+  echo "libhdfs3_install_pkg_${1}_${git_hash}.tgz"
+}
+
 _main() {
   mkdir build
   pushd build
   ../bootstrap
-  make
+  local parent_dir
+  parent_dir=$(dirname "$(pwd)")
+  local dest="package"
+  mkdir ${dest}
+  make DESTDIR=${dest} install
+  ensure_git_installed
+
+  local dir_to_tar
+  dir_to_tar="${dest}${parent_dir}/dist"
+
+  local version_number
+  version_number=$(get_libhdfs3_version "${dir_to_tar}")
+
+  local s3_package
+  s3_package=$(generate_package_name "${version_number}")
+
+  echo "${s3_package}"
+  tar -cvzf "${s3_package}" "${dir_to_tar}"
 }
 
 _main "$@"
