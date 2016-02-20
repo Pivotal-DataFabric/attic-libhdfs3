ea9b80904e438c29b3231c55f1c20ef928e09f44
Jesse Zhang <jzhang@pivotal.io>
2016-02-19 16:13:35 -0800

    Improve libhdfs3 compilation job
    - Had been failing for various reasons, we ended up with what hopefully
      is a clearer input-to-output flow

--- ci/compile.bash
+++ ci/compile.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euxo pipefail
 
 get_libhdfs3_version() {
   local dir_to_tar
@@ -8,34 +8,57 @@ get_libhdfs3_version() {
   cat "${dir_to_tar}/lib/pkgconfig/libhdfs3.pc" | grep Version | cut -d ' ' -f2
 }
 
-generate_package_name() {
+get_git_hash() {
+  local git_root_dir="$1"
   local git_hash
-  git_hash=$(git log -1 --oneline | head -1 | cut -d ' ' -f1)
-  echo "libhdfs3_install_pkg_${1}_${git_hash}.tgz"
+
+  pushd "$git_root_dir" &>/dev/null
+    git_hash="$(git log -1 --oneline | cut -d ' ' -f1)"
+  popd &>/dev/null
+
+  echo "$git_hash"
+}
+
+generate_package_name() {
+  local version_number="$1"
+  local git_hash="$2"
+  echo "libhdfs3_install_pkg_${version_number}_${git_hash}.tgz"
 }
 
 _main() {
-  mkdir build
-  pushd build
-  ../bootstrap
-  local parent_dir
-  parent_dir=$(dirname "$(pwd)")
-  local dest="package"
-  mkdir ${dest}
-  make DESTDIR=${dest} install
+  #Concourse mounts a volume at the output directory, so we expect that 'build' is already here
+  local output_dir="$1"
+  local install_destination="${output_dir}/package"
 
-  # dir_to_tar's root should be dist 
+  mkdir -p "$install_destination"
+
+  local src_dir
+  #Providing the full expanded path here because it's going to be concatenated in an ugly way later
+  src_dir="$(pwd)/libhdfs3_src"
+
+  pushd "$output_dir"
+    "$src_dir"/bootstrap
+    make DESTDIR="../$install_destination" install
+  popd
+
+  # the bootstrap script will have copied over many files deep into the 'package' directory
+  # recreating the full src_dir directory structure
+  # and then dir_to_tar's root should be 'dist' because make puts the compiled parts into 'dist'
   local dir_to_tar
-  dir_to_tar="${dest}${parent_dir}/dist"
+  dir_to_tar="${install_destination}${src_dir}/dist"
 
   local version_number
   version_number=$(get_libhdfs3_version "${dir_to_tar}")
 
-  local s3_package
-  s3_package=$(generate_package_name "${version_number}")
+  local git_hash
+  git_hash=$(get_git_hash "${src_dir}")
 
-  echo "${s3_package}"
-  tar -cvzf "${s3_package}" -C "${dir_to_tar}" .
+  local s3_package
+  s3_package=$(generate_package_name "${version_number}" "$git_hash")
+
+  # make the tarball from the perspective of the dist directory
+  # put it in the mounted output directory that concourse provided
+  tar -cvzf "${output_dir}/${s3_package}" -C "${dir_to_tar}" .
 }
 
-_main "$@"
+_main $OUTPUT_PATH "$@"
